70f566d2b7627424c70a3ea9a1f1be6d0cfc6989
hss01248 <hss012489@gmail.com>
2021-12-31 17:47:38 +0800

    删除无用脚本

--- deps/deps-updates-debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# 在项目根目录下运行: sh deps/deps-updates-debug.sh
-
-echo "当前目录:"$PWD
-
-echo "debug依赖更新一览表(比较耗时)"
-gradlew dependencyUpdates -Drevision=debug -DoutputFormatter=html -DoutputDir=../deps/debug -DreportfileName=debug-deps-updates
