eabd740adc4b3ea0e9d40f4e8a50a5f31eace5ca
hss01248 <hss012489@gmail.com>
2023-10-18 16:18:22 +0800

    feat(sentry): okhttp代理urlconnection放到最前初始化,调试sentryio

---
+++ network-urlconnection/src/main/java/com/hss01248/flipper/urlconnection/ProxyUrlInit2.java
@@ -0,0 +1,51 @@
+package com.hss01248.flipper.urlconnection;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * @Despciption todo
+ * @Author hss
+ * @Date 18/10/2023 15:27
+ * @Version 1.0
+ */
+public class ProxyUrlInit2  extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        new InitForUrlConnection().create(null);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
